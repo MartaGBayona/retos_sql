c7c73c4a00b542c76b5a5f480ae906bd60d037db
MartaGBayona <marta.g.bayona@outlook.es>
2024-02-21 13:51:28 +0100

    create archives

--- Reto-2/reto2.sql
+++ Reto-2/reto2.sql
@@ -1,2 +1,49 @@
 -- Diseña un sistema para gestionar reservas en restaurantes. Los usuarios pueden buscar restaurantes, hacer reservas para ciertas fechas y horas, dejar reseñas sobre sus experiencias y acumular puntos de fidelidad. ¿Cuáles serían las entidades clave?
 
+CREATE DATABASE reto_2;
+
+USE reto_2;
+
+CREATE TABLE users(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50),
+    email VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(200) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE restaurants(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    name INT NOT NULL,
+    user_id INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
+CREATE TABLE bookings (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    restaurant_id INT,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(restaurant_id) REFERENCES restaurants(id)
+);
+
+CREATE TABLE reviews (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(50) NOT NULL,
+    description VARCHAR(300),
+    value INT NOT NULL,
+    user_id INT,
+    restaurant_id INT,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(restaurant_id) REFERENCES restaurants(id)
+);
